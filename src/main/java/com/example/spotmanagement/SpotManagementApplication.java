bb03d8ddcc862e35abf24b114fc4bfa12135c772
鈴木舞 <suzukimai@suzukimainoMacBook-Air.local>
2026-04-19 13:37:40 +0900

    feat: 初期プロジェクト作成

---
+++ src/main/java/com/example/spotmanagement/SpotManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spotmanagement/SpotManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
